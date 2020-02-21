86bcbd50e6f1698ded83ec02cdafb3bb33eb7d01
Brad Trantham <trantham@gmail.com>
2020-02-21 17:14:02 -0600

    Commit after lecture 25

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  // equivalent to:
+  // function goalInputHandler(enteredText) {
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    // currentGoals is just a pointer to the latest state that setCourseGoals has access to
+    // the anonymous function notation is short for {return [...currentGoals, enteredGoal];}
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
+
   return (
-    <View  style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}/>
-        <Button title="Add"/>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" 
+                   style={styles.input} 
+                   onChangeText={goalInputHandler}
+                   value={enteredGoal}/>
+        <Button title="Add" onPress={addGoalHandler}/>
       </View>
       <View></View>
     </View>
@@ -14,5 +32,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  screen : {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  }
 });
